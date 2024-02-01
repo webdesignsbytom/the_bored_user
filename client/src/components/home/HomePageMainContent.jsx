7fc192de96585dbef7e4aeae58aaa338f02d0cc7
Thomas Brockington <xtombrock1989@gmail.com>
2024-02-01 22:03:35 +0000

    wqf

--- client/src/components/home/HomePageMainContent.jsx
+++ client/src/components/home/HomePageMainContent.jsx
@@ -1,31 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function HomePageMainContent({ allArticles, articleImagesEndPoint }) {
+  const [article, setArticle] = useState([]);
+  console.log('article', article);
+
+  useEffect(() => {
+    setArticle(allArticles[0]);
+  }, []);
+
   return (
     <div className='grid h-full w-full'>
+      <div>Article</div>
       <div>
-        {allArticles.map((article, index) => {
-          return (
-            <article key={index}>
-              <div>
-                <h6>{article.articleTitle}</h6>
-              </div>
-              <div>
-                <span>{article.articleAuthor}</span>
-              </div>
-              <div>
-                {article.articleTags.map((tag, index) => {
-                    return (
-                        <span key={index}>{tag}</span>
-                    )
-                })}
-              </div>
-              <div>
-                <img src={articleImagesEndPoint + article.articleHeaderImages[0].imageUrl} alt="" />
-              </div>
-            </article>
-          );
-        })}
+
       </div>
     </div>
   );
